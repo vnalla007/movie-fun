645a5c05248a4eb5419d5a9d6d067366685a75d3
venkata Cognizant Git <venkatasivaramakrishna.nallagorla@cognizant.com>
2020-03-18 21:57:28 -0500

    changed for removing filepath for file read

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -3,8 +3,8 @@ package org.superbiz.moviefun;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +14,9 @@ public class CsvUtils {
 
     public static String readFile(String path) {
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            ClassLoader classLoader = CsvUtils.class.getClassLoader();
+            Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,7 +24,7 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
